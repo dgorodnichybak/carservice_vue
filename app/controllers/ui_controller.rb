6a12a65321dff5968c6f930b40773f6d35c9a150
Dmitry Gorodnichy <dgorodnichy@shakuro.com>
2017-10-28 18:52:12 +0300

    Style sign in form

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,18 +1,19 @@
 class UiController < ApplicationController
   def index
-      render html:
-        '<!DOCTYPE html>'\
-        '<html lang="en">'\
-            '<head>'\
-                '<meta charset="utf-8" />'\
-                '<meta name="viewport" content="width=device-width" />'\
-                '<title>CarService</title>'\
-                '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">'\
-                "#{ActionController::Base.helpers.javascript_pack_tag 'application'}"\
-            '</head>'\
-            '<body>'\
-                '<div id="app"></div>'\
-            '</body>'\
-        '</html>'.html_safe
-    end
+    render html:
+      '<!DOCTYPE html>'\
+      '<html lang="en">'\
+        '<head>'\
+          '<meta charset="utf-8" />'\
+          '<meta name="viewport" content="width=device-width" />'\
+          '<title>CarService</title>'\
+          '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css">'\
+          "#{ActionController::Base.helpers.javascript_pack_tag 'application'}"\
+          "#{ActionController::Base.helpers.stylesheet_pack_tag 'application'}"\
+        '</head>'\
+        '<body>'\
+          '<div id="app"></div>'\
+        '</body>'\
+      '</html>'.html_safe
+  end
 end
